7a92d32bc1a2aa8e5500ae001571fcbd4307adb3
Glitch (glitch-hello-website) <none>
2022-06-30 21:56:58 +0000

    📫🍅 Checkpoint ./index.html:42900803/11397 ./style.css:42900803/7081 ./script.js:42900803/1456 ./.glitch-assets:42900803/1489

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-/*
-This is your site JavaScript code - you can add interactivity and carry out processing
-- Initially the JS writes a message to the console, and moves a button you can add from the README
-*/
+let text= "Eres único. Rolex.";
+let i=0;
+let speed= 150;
+function type(){
+    if(i<text.length){
+        document.querySelector('#par').textContent+=text.charAt(i);
+        i++;
+        setTimeout(type, speed);
 
-// Print a message in the browser's dev tools console each time the page loads
-// Use your menus or right-click / control-click and choose "Inspect" > "Console"
-console.log("Hello 🌎");
-
-/* 
-Make the "Click me!" button move when the visitor clicks it:
-- First add the button to the page by following the "Next steps" in the README
-*/
-const btn = document.querySelector("button"); // Get the button from the page
-// Detect clicks on the button
-if (btn) {
-  btn.onclick = function() {
-    // The JS works in conjunction with the 'dipped' code in style.css
-    btn.classList.toggle("dipped");
-  };
+    }
 }
+type();
 
-// This is a single line JS comment
-/*
-This is a comment that can span multiple lines 
-- use comments to make your own notes!
-*/
+const items= document.querySelectorAll('.item');
+
+items.forEach(item=>{
+    item.addEventListener('mouseover', ()=>{
+        removeFocus();
+        item.classList.add('selected');
+    })
+
+    removeFocus =()=>{
+        items.forEach(item=>{
+            item.classList.remove('selected');
+        })
+    }
+})
